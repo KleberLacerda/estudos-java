5a5a53f221670c6f9804adb67e1580a7e27f27ae
Kleber Lacerda <kl.lacerda@gmail.com>
2026-07-16 20:52:15 -0300

    feat: atualizar o readme e o gerador de senhas

--- src/GeradorDeSenhasSeguras.java
+++ src/GeradorDeSenhasSeguras.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.security.SecureRandom;
+import java.util.Scanner;
 
 public class GeradorDeSenhasSeguras {
+
+    private static final String CARACTERES = "!@#$%&*()+{}[]?.,;/1234567890qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM";
+
+    public static String gerarSenha(int tamanho){
+        SecureRandom geradorAleatorio = new SecureRandom();
+        StringBuilder senha = new StringBuilder(tamanho);
+
+        for (int i = 0; i < tamanho; i++) {
+            int indice = geradorAleatorio.nextInt(CARACTERES.length());
+            senha.append(CARACTERES.charAt(indice));
+        }
+        return senha.toString();
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o tamanho da senha que deseja criar: ");
+        int tamanhoSenha = scanner.nextInt();
+
+        String senha = gerarSenha(tamanhoSenha);
+        System.out.println("Senha gerada: " + senha);
+    }
 }
